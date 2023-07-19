362d571cbd18780d99001dda554f4d48b1751881
Dmitry Gorschkov <dmgorschkov@mail.ru>
2023-07-19 11:05:43 +0300

    Close issue #1

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+	roots: ['<rootDir>/src'],
+	transform: {
+		'^.+\\.tsx?$': 'ts-jest',
+	},
+	testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+	testTimeout: 20000, // Установка таймаута в 10 секунд для всех тестов
+	verbose: true,
+};
