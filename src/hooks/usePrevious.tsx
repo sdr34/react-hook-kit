bdbda4158692f4d15991eec37fd8dfce924f07c1
Dima <drivermars02@gmail.com>
2023-07-26 09:12:14 +0200

    Initial commit

---
+++ src/hooks/usePrevious.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { useRef, useEffect } from 'react';
+
+function usePrevious<T>(value: T): T | undefined {
+  const ref = useRef<T>();
+
+  useEffect(() => {
+    ref.current = value;
+  }, [value]);
+
+  return ref.current;
+}
+
+export default usePrevious;
+
